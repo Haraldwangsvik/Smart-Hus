bb9eb918db196cc981d274028b3309a7fd078f42
Harald <haraldwf@stud.ntnu.no>
2022-12-11 14:44:38 +0100

    Create class UserInterface

---
+++ SmartHus/src/main/java/no/ntnu/idata1001/UserInterface.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata1001;
+
+/**
+ * Represents the ItemRegister application, including the dialog with
+ * the user (input and output)
+ *
+ * @author Harald W Fredriksen
+ * @version 11.12.2022
+ */
+public class UserInterface {
+
+
+}
